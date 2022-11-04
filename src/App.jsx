cb0d98f3a039f9cbd14948469dceb6a37ee79597
kennokybar <kenno.kybar@gmail.com>
2022-11-04 10:52:08 +0200

    Pokemonid

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,56 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Pais from './Pais'
-import IlmaInfo from './IlmaInfo'
 import './App.css'
-
+import PokemoniInfo from './PokemoniInfo'
+import PokemonideList from './PokemonideList'
 
 function App() {
+  const [valitudPokemon, setValitudPokemon] = useState()
+  const [pokemonid, setPokemonid] = useState([])
+  const [eelmineUrl, setEelmineUrl] = useState(null)
+  const [jargmineUrl, setJargmineUrl] = useState(null)
+
+  useEffect(() => {
+    pariPokemonid('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20')
+  }, [])
+
+  const pariPokemonid = async (url) => {
+    if (!url) return
+    const laetudPokemonid = await (await fetch(url)).json()
+    console.log(laetudPokemonid)
+    setPokemonid(laetudPokemonid.results)
+    setEelmineUrl(laetudPokemonid.previous)
+    setJargmineUrl(laetudPokemonid.next)
+  }
+
+  const pariPokemoniInfo = async (url) => {
+    const pokemoniInfo = await fetch(url)
+    setValitudPokemon(await pokemoniInfo.json())
+  }
+  console.log(valitudPokemon && valitudPokemon.id)
+  if (valitudPokemon != null) return (
+    <div className="App">
+      <Pais />
+      <a onClick={() => { setValitudPokemon(undefined) }}>Kustuta valitud pokemon</a>
+      <br />
+      <br />
+      <PokemoniInfo pokemon={valitudPokemon} />
+      <div onClick={() => { pariPokemoniInfo(`https://pokeapi.co/api/v2/pokemon/${valitudPokemon.id - 1}`) }}>Eelmine</div>
+      <div onClick={() => { pariPokemoniInfo(`https://pokeapi.co/api/v2/pokemon/${valitudPokemon.id + 1}`) }}>Järgmine</div>
+    </div>
+  )
 
   return (
     <div className="App">
       <Pais />
-      <IlmaInfo />
+      <PokemonideList pokemonid={pokemonid} pariPokemoniInfo={pariPokemoniInfo} />
+      <br />
+      <br />
+      <div>
+        {eelmineUrl ? <span onClick={() => { pariPokemonid(eelmineUrl) }}>{'<'} Eelmine leht</span> : null}
+        <span> | </span>
+        {jargmineUrl ? <span className="jargmineNupp" onClick={() => { pariPokemonid(jargmineUrl) }}>Järgmine leht {'>'}</span> : null}
+      </div>
     </div>
   )
 }
